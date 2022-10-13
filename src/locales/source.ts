87dacff31f99aaf5f11979890752aee8d2807ae7
John-Oula <johnoula@antratechstudios.com>
2022-10-13 13:25:44 +0800

    new resource files

---
+++ src/locales/source.ts
@@ -0,0 +1,218 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en">
+  <context>
+    <message>
+      <source>feature_body</source>
+      <translation>We\'re here to help you connect, communicate, and express your ideas so you can get more done together. We\'re proud to be trusted by millions of large enterprises, small businesses, and individuals. </translation>
+    </message>
+    <message>
+      <source>feature_heading</source>
+      <translation>Never miss a meeting</translation>
+    </message>
+    <message>
+      <source>feature_label_title</source>
+      <translation>Our Product</translation>
+    </message>
+    <message>
+      <source>feature_title_1</source>
+      <translation>Team Collaboration</translation>
+    </message>
+    <message>
+      <source>feature_title_2</source>
+      <translation>Cloud Video Recording</translation>
+    </message>
+    <message>
+      <source>feature_title_3</source>
+      <translation>Third-party integrations</translation>
+    </message>
+    <message>
+      <source>footer_menu_option_1</source>
+      <translation>Overview</translation>
+    </message>
+    <message>
+      <source>footer_menu_option_2</source>
+      <translation>Features</translation>
+    </message>
+    <message>
+      <source>footer_menu_option_3</source>
+      <translation>Features</translation>
+    </message>
+    <message>
+      <source>footer_menu_option_4</source>
+      <translation> Pricing</translation>
+    </message>
+    <message>
+      <source>footer_menu_option_5</source>
+      <translation>Releases</translation>
+    </message>
+    <message>
+      <source>footer_menu_option_6</source>
+      <translation>Partners</translation>
+    </message>
+    <message>
+      <source>footer_menu_title</source>
+      <translation>Product</translation>
+    </message>
+    <message>
+      <source>hero_body</source>
+      <translation>Never miss a meeting. Never be late for one too. Keep track of your meetings and receive smart reminders in appropriate times. Read your smart “Daily Agenda” every morning.</translation>
+    </message>
+    <message>
+      <source>hero_button_text_1</source>
+      <translation>Get Started</translation>
+    </message>
+    <message>
+      <source>hero_button_text_2</source>
+      <translation>Learn More</translation>
+    </message>
+    <message>
+      <source>hero_title_1</source>
+      <translation>Meeting scheduling</translation>
+    </message>
+    <message>
+      <source>hero_title_2</source>
+      <translation>made easy</translation>
+    </message>
+    <message>
+      <source>homepage_new_cta</source>
+      <translation>Work with us!</translation>
+    </message>
+    <message>
+      <source>homepage_new_featured_projects</source>
+      <translation>Featured Projects.</translation>
+    </message>
+    <message>
+      <source>homepage_new_hero_body_text</source>
+      <translation>We are a design technology company specialized in delivering high-end custom web applications for businesses using industry leading technologies.Watch us turn your idea to a working product that will mirror your brand.</translation>
+    </message>
+    <message>
+      <source>homepage_new_hero_text_1</source>
+      <translation>Design.</translation>
+    </message>
+    <message>
+      <source>homepage_new_hero_text_2</source>
+      <translation>Develop.</translation>
+    </message>
+    <message>
+      <source>homepage_new_hero_text_3</source>
+      <translation>Deliver.</translation>
+    </message>
+    <message>
+      <source>homepage_new_services_details_1</source>
+      <translation>Details</translation>
+    </message>
+    <message>
+      <source>homepage_new_services_details_2</source>
+      <translation>Details</translation>
+    </message>
+    <message>
+      <source>homepage_new_services_details_3</source>
+      <translation>Details</translation>
+    </message>
+    <message>
+      <source>homepage_new_services_details_4</source>
+      <translation>Details</translation>
+    </message>
+    <message>
+      <source>homepage_new_services_service_description_1</source>
+      <translation>End-to-end Development that will get your project launched seamlessly for your business to leverage in due time from start to finish.</translation>
+    </message>
+    <message>
+      <source>homepage_new_services_service_description_2</source>
+      <translation>Get eye-catching and top tier UI Designs that will speak for your brand.First impression is everything.</translation>
+    </message>
+    <message>
+      <source>homepage_new_services_service_description_3</source>
+      <translation>Today\'s applications depend on third-party API\'s for data that plays a big role in order to meet the business\' goals.</translation>
+    </message>
+    <message>
+      <source>homepage_new_services_service_description_4</source>
+      <translation>Boost your sales dramatically through taking your business online and reaching a larger audience.Nothing better than more sales.</translation>
+    </message>
+    <message>
+      <source>homepage_new_services_service_title_1</source>
+      <translation>Web Development</translation>
+    </message>
+    <message>
+      <source>homepage_new_services_service_title_2_1</source>
+      <translation>UI</translation>
+    </message>
+    <message>
+      <source>homepage_new_services_service_title_2_2</source>
+      <translation>Design</translation>
+    </message>
+    <message>
+      <source>homepage_new_services_service_title_3_1</source>
+      <translation>API</translation>
+    </message>
+    <message>
+      <source>homepage_new_services_service_title_3_2</source>
+      <translation>Integration</translation>
+    </message>
+    <message>
+      <source>homepage_new_services_service_title_4</source>
+      <translation>Ecommerce Development</translation>
+    </message>
+    <message>
+      <source>homepage_new_view_work</source>
+      <translation>View Work </translation>
+    </message>
+    <message>
+      <source>login</source>
+      <translation>login</translation>
+    </message>
+    <message>
+      <source>navbar_item_1</source>
+      <translation>Home</translation>
+    </message>
+    <message>
+      <source>navbar_item_2</source>
+      <translation>Features</translation>
+    </message>
+    <message>
+      <source>navbar_item_3</source>
+      <translation>Pricing</translation>
+    </message>
+    <message>
+      <source>navbar_item_4</source>
+      <translation>Download</translation>
+    </message>
+    <message>
+      <source>navbar_item_5</source>
+      <translation>Help Center</translation>
+    </message>
+    <message>
+      <source>signup</source>
+      <translation>signup</translation>
+    </message>
+    <message>
+      <source>stats_heading</source>
+      <translation>Statistics</translation>
+    </message>
+    <message>
+      <source>stats_title_1</source>
+      <translation>Users</translation>
+    </message>
+    <message>
+      <source>stats_title_2</source>
+      <translation>Downloads</translation>
+    </message>
+    <message>
+      <source>stats_title_3</source>
+      <translation>Years</translation>
+    </message>
+    <message>
+      <source>stats_value_1</source>
+      <translation>50K+</translation>
+    </message>
+    <message>
+      <source>stats_value_2</source>
+      <translation>1M+</translation>
+    </message>
+    <message>
+      <source>stats_value_3</source>
+      <translation>20</translation>
+    </message>
+  </context>
+</TS>
